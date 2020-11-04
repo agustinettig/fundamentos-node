e270897c3e4ec38ac9038f876b7d7901ac30be7c
agustinettig <40875912+agustinettig@users.noreply.github.com>
2020-11-03 21:02:27 -0300

    criando funcionalidade para os endpoints

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,27 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (total, transaction) => {
+        if (transaction.type === 'income') total.income += transaction.value;
+        else total.outcome += transaction.value;
+        return total;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(transaction: Transaction): Transaction {
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
